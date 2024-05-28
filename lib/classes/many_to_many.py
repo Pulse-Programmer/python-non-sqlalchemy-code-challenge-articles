d9cfca63ba4d7bf9c87de626f8e75c2bee9f2c4b
Stephen Ochieng <materiamystic@gmail.com>
2024-05-28 15:27:12 +0300

    Adds required methods

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,102 @@
 class Article:
+    
+    all = []
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
+        Article.all.append(self)
         
+        
+    @property
+    def title(self):
+       return self._title
+   
+   
+    @title.setter
+    def title(self, title):
+       if type(title) in (str,) and (5<=len(title)<=50) and (hasattr(self,"title")==False):
+           self._title = title
+   
+   
+   
 class Author:
     def __init__(self, name):
-        self.name = name
+        self.name = name 
 
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if type(name) in (str,) and (len(name)>0) and (hasattr(self,"name")==False):
+            self._name = name
+    
     def articles(self):
-        pass
-
+        return [article for article in Article.all if article.author == self]
+        
     def magazines(self):
-        pass
-
+        return list(set(article.magazine for article in self.articles()))
+        
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
+       
 
     def topic_areas(self):
-        pass
+        topics = (set(magazine.category for magazine in self.magazines()))
+        if len(topics) == 0:
+            return None
+        else:
+            return list(topics)
 
 class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        # self._validate_name(name)
 
+
+    @property
+    def name(self):
+        return self._name
+    
+    
+    @name.setter
+    def name(self, name):
+        if type(name) in (str,) and (2<=len(name)<=16):
+            self._name = name
+            
+    
+    @property
+    def category(self):
+        return self._category
+    
+    
+    @category.setter
+    def category(self, category):
+        if type(category) in (str,) and len(category)>0:
+            self._category = category
+       
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list(set(article.author for article in self.articles()))
 
     def article_titles(self):
-        pass
+        titles = [article.title for article in self.articles()]
+        if len(titles) == 0:
+            return None
+        else:
+            return titles
 
     def contributing_authors(self):
-        pass
+        authors = (set(article.author for article in self.articles()))
+        moreThanTwoArticles = [author for author in authors if sum(1 for art in self.articles() if art.author == author) > 2]
+        if len(moreThanTwoArticles) == 0:
+            return None
+        else:
+            return moreThanTwoArticles
+        
